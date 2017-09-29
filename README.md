628148935874ac4a6b302dd61935853d40605e4b
Marcus Larsen <marcuslarsen97@gmail.com>
2017-09-29 18:02:52 +0200

    Initial commit
    Add .gitignore, README.md, bin/dba.sh, dotfiles/bash_alias, dotfiles/bash_loginart, dotfiles/bashrc, dotfiles/vimrc

---
+++ README.md
@@ -0,0 +1,6 @@
+slet den anden.
+slet repo på GitHub
+omdøb mappen ./
+lav ny git repo
+lav ny github repo
+skriv en ordentlig README.md :^)
---
+++ bin/dba.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Søger dba.dk og printer resultater.
+
+# Print help and exit if --help option or none at all is given
+if [[ -z "$@" || "$1" = "--help" ]]; then
+	echo "Usage: dba.sh [ARGUMENT]..."
+	echo
+	echo "Search dba.dk for each argument and print results."
+	echo "With --help option: print this and exit."
+	echo "By Marcus Larsen"
+	exit 0
+fi
+
+# Iterate arguments
+for ARG in "$@"; do
+	echo "Search: $ARG"
+
+	# Replace spaces with +
+	SEARCH="${ARG// /+}"
+
+	# Search dba.dk
+	RESULTS="$(curl -s https://www.dba.dk/soeg/?soeg="$SEARCH" | grep --color=never "class=\"listingLink\"")"
+
+	# Echo results if any
+	if [[ ! -z "$RESULTS" ]]; then
+		echo $RESULTS
+	else
+		echo "None"
+	fi
+	echo
+done
+
+exit 0
