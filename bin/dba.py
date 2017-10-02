75884013ad43946e9421b8eb1cdc136e23b5e0af
Marcus Larsen <marcuslarsen97@gmail.com>
2017-10-02 16:11:12 +0200

    Update dba.py to finished version

--- bin/dba.py
+++ bin/dba.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
 
-import argparse
+import sys
 import requests
 from bs4 import BeautifulSoup
 import json
 
 baseurl = "https://www.dba.dk/soeg/?soeg="
 
-# (argparse) read search query from command line arguments
-# (requests) get .html from dba.dk
-# (bs4) search .html for class listingLink
-# print results (article, price, date, link)
+args = sys.argv[1:]
 
-args = ['bastl', 'monotron', 'volca']
+if not args or args[0] == '--help':
+    print('Usage: dba.py [--help] [ARGUMENT]...')
+    print('')
+    print('Search dba.dk for each argument and print results.')
+    print('With --help option: print this and exit.')
+    print('By Marcus Larsen')
+else:
+    for arg in args:
+        print('Search: ' + arg)
+        url = baseurl + arg
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
 
-for arg in args:
-    print('Search: ' + arg)
-    url = baseurl + arg
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
+        count = 0
+        for td in soup.find_all('td'):
+            if td.get('class') and 'mainContent' in td.get('class'):
+                count += 1
+                data = json.loads(td.script.string)
 
-    count = 0
-    for td in soup.find_all('td'):
-        if td.get('class') and 'mainContent' in td.get('class'):
-            count += 1
-            data = json.loads(td.script.string)
+                print(data['name'])
+                print('url:\t' + data['url'])
+                print('price:\t' + data['offers']['price'] + data['offers']['priceCurrency'])
+                print('')   # newline
 
-            print(data['name'])
-            print('url:\t' + data['url'])
-            print('price:\t' + data['offers']['price'] + data['offers']['priceCurrency'])
-            print('')   # newline
-
-    if count == 0:
-        print('None\n')
+        if count == 0:
+            print('None\n')
