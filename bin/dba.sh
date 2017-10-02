db7479131a6cc26a85eb8e41eefa16ae8cffc89d
Marcus Larsen <marcuslarsen97@gmail.com>
2017-10-02 15:44:29 +0200

    Minor syntax update

--- bin/dba.sh
+++ bin/dba.sh
@@ -2,7 +2,8 @@
 # Søger dba.dk og printer resultater.
 
 # Print help and exit if --help option or none at all is given
-if [[ -z "$@" || "$1" = "--help" ]]; then
+if [[ -z "$@" || "$1" = "--help" ]]
+then
 	echo "Usage: dba.sh [ARGUMENT]..."
 	echo
 	echo "Search dba.dk for each argument and print results."
@@ -12,7 +13,8 @@ if [[ -z "$@" || "$1" = "--help" ]]; then
 fi
 
 # Iterate arguments
-for ARG in "$@"; do
+for ARG in "$@"
+do
 	echo "Search: $ARG"
 
 	# Replace spaces with +
@@ -22,7 +24,8 @@ for ARG in "$@"; do
 	RESULTS="$(curl -s https://www.dba.dk/soeg/?soeg="$SEARCH" | grep --color=never "class=\"listingLink\"")"
 
 	# Echo results if any
-	if [[ ! -z "$RESULTS" ]]; then
+	if [[ ! -z "$RESULTS" ]]
+	then
 		echo $RESULTS
 	else
 		echo "None"
